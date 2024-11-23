b66838a0e43318bfeab4ef883830a0dc3524ab2a
kmeng256 <kmeng@berkeley.edu>
2024-11-23 14:37:19 -0800

    m

--- FOOD__PANTRY/HomeView.swift
+++ FOOD__PANTRY/HomeView.swift
@@ -8,7 +8,7 @@
 //hihihihi/
 import SwiftUI
 
-struct draftHomeView: View {
+struct HomeView: View {
     
     var pantryManager: PantryManager
     
@@ -21,7 +21,7 @@ struct draftHomeView: View {
                         .font(.system(size: 35))
                         .foregroundColor(.black)
                         .bold()
-                        // .italic()
+                    // .italic()
                     Text("Pantry")
                         .font(.system(size: 35))
                         .foregroundColor(.black)
@@ -71,116 +71,8 @@ struct draftHomeView: View {
             // use similar view from cartView
         }
     }
-    
-    
-
-
-//    var myTypes: [Any] = [View1.self, View2.self]
-//        var body: some View {
-//            List {
-//                ForEach(0..<myTypes.count) { index in
-//                    self.buildView(types: self.myTypes, index: index)
-//                }
-//            }
-//        }
-    
-//    var body: some View {
-//        NavigationStack {
-//            VStack(spacing: 30) {
-//                TitleTextFieldView(title: "Owner Name", contentText: $ownerName)
-//                VStack(alignment: .leading, spacing: 10) {
-//                    TitleTextFieldView(title: "Card Number", contentText: $cardNumber)
-//                    generateCreditCardNumberButton
-//
-//                }
-//                Spacer()
-//                addCreditCardButton
-//            }
-//            .navigationTitle("Add New Card")
-//            .navigationBarTitleDisplayMode(.inline)
-//            .padding()
-//            .toolbar {
-//                ToolbarItem(placement: .topBarLeading) {
-//                    Button(action: {
-//                        dismiss()
-//                    }) {
-//                        Image(systemName: "xmark.circle.fill")
-//                            .foregroundStyle(.gray)
-//                            .font(.system(size: 25))
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    private var addCreditCardButton: some View {
-//        // TODO: 3A. Implement addCreditCardButton
-//
-//        Button(action: {
-//            addCreditCard()
-//            dismiss()
-//            }) {
-//            ZStack {
-//                RoundedRectangle(cornerRadius: 8)
-//                    .fill(Color.blue)
-//                    .frame(height: 50, alignment: .leading)
-//                Text("Add Credit Card!")
-//                    .foregroundColor(.white)
-//            }
-//            // dismiss()
-//        }
-//    }
-//
-//    private var generateCreditCardNumberButton: some View {
-//        // TODO: 3B. Implement generateCreditCardNumberButton
-//        Button(action: generateCreditCardNumber) {
-//            ZStack {
-//                RoundedRectangle(cornerRadius: 8)
-//                    .fill(Color.green)
-//                    .frame(height: 50, alignment: .leading)
-//                HStack {
-//                    Image(systemName: "creditcard.and.123") // insert image
-//                        .foregroundColor(Color.white)
-//                    Text("Generate Credit Card Number")
-//                        .foregroundColor(.white)
-//                }
-//            }
-//        }
-//        // generateCreditCardNumber()
-//    }
-//
-//    private func generateCreditCardNumber() {
-//        cardNumber = cardManager.createCreditCardNumber()
-//    }
-//
-//    private func addCreditCard() {
-//        let cardy: CFCard = CFCard(cardNumber: cardNumber, ownerName: ownerName, balance: 0, transactions: [])
-//        cardManager.addCard(for: cardy)
-//    }
 }
 
-/// A helper view that customizes a `TextField` by adding an associated title at the top as well as some UI customization.
-//struct TitleTextFieldView: View {
-//    var title: String
-//    @Binding var contentText: String
-//    var body: some View {
-//        VStack(alignment: .leading) {
-//            Text(title)
-//                .font(.title2)
-//                .bold()
-//            RoundedRectangle(cornerRadius: 10)
-//                .fill(.blue.opacity(0.1))
-//                .stroke(.blue, lineWidth: 1)
-//                .frame(height: 50)
-//                .overlay(
-//                    TextField("Enter \(title)", text: $contentText)
-//                        .padding(10)
-//                        .bold()
-//                )
-//        }
-//    }
-//}
-
 #Preview {
-    draftHomeView(pantryManager: PantryManager())
+    HomeView(pantryManager: PantryManager())
 }
