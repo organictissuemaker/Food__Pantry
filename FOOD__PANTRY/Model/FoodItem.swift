97b32911135a72f56d55b20daedf74c2ab7bed48
kmeng256 <kmeng@berkeley.edu>
2024-11-23 17:46:17 -0800

    item

--- FOOD__PANTRY/Model/FoodItem.swift
+++ FOOD__PANTRY/Model/FoodItem.swift
@@ -19,12 +19,14 @@ import Foundation
     var id = UUID()
     var name: String
     var stock: Int
+    var quantity: Int
     var image: String
     
     init(id: UUID = UUID(), name: String, stock: Int, image: String, type: foodCategory) {
         self.id = id
         self.name = name
         self.stock = stock
+        self.quantity = 0
         self.image = image
         self.type = type
     }
